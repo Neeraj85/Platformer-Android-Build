0dc64b87449a52092c2e5b279c2f43f293fbd5f0
Neeraj85 <neerajshardul@gmail.com>
2024-09-06 13:10:31 +0530

    Added android export presets, navigation keys, menu backgrounds and buttons, etc

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -3,6 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Global.lives = 3
 	$Timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
